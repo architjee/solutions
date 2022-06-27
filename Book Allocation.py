fa3c539a20856bcec3c029832b5221d1edc69309
architjee <32292295+architjee@users.noreply.github.com>
2022-06-27 16:37:23 +0530

    Finally did Book Allocation.py

--- Book Allocation.py
+++ Book Allocation.py
@@ -12,32 +12,23 @@ def isValid(listA, n, m, mxp):
         return True
     return False;
 t = int(input())
-n,m = map(int, input().split())
-listA =list( map(int, input().split()));
-high = (sum(listA));
-# print(listA)
-mid=0;
-low = max(listA);
-while low<=high:
-    mid =( low+ high)//2;
-    if isValid(listA, n, m, mid):
-        # We can go for lower
-        high = mid-1;
-    else:
-        # We can go for higher
-        low = mid+1;
-# Now max allowed sum is mid.
-summation  = 0;
-maxSoFar = 0;
-students = 0;
-i=0;
-print("here ", mid)
-summation = 0;
-while(i<n):
-    summation += listA[i];
-    if(summation>mid):
-        students += 1;
-        summation = listA[i];
-    maxSoFar = max(summation, maxSoFar)
-    i+=1
-print(maxSoFar);
+for _ in range(t):
+        
+    n,m = map(int, input().split())
+    listA =list( map(int, input().split()));
+    high = (sum(listA));
+    # print(listA)
+    mid=0;
+    result = -1;
+    low = max(listA);
+    while low<=high:
+        mid =( low+ high)//2;
+        if isValid(listA, n, m, mid):
+            # We can go for lower
+            result =mid
+            high = mid-1;
+        else:
+            # We can go for higher
+            low = mid+1;
+    # Now max allowed sum is mid.
+    print(result)
