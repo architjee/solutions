df5104056ae44c3a62ab38d5d203dc1bce1d66d9
Archit Jain <architjee@gmail.com>
2022-07-24 19:25:30 +0530

    Refactoring The file

--- String Sort.py
+++ String Sort.py
@@ -1,15 +1,15 @@
 # Program for coding blocks question String Sort
 
-n = int(input());
+n = int(input())
 bigList = []
 for i in range(n):
-    bigList.append(input());
+    bigList.append(input())
 # Implementing bubble sort.
 for i in range(n):
-    for j in range(i+1, n):
-        if(bigList[j].find(bigList[j-1])!=-1):
-            bigList[j] , bigList[j-1] = bigList[j-1], bigList[j]
+    for j in range(i + 1, n):
+        if bigList[j].find(bigList[j - 1]) != -1:
+            bigList[j], bigList[j - 1] = bigList[j - 1], bigList[j]
             continue
-        if(bigList[j-1]>bigList[j]):
-            bigList[j-1], bigList[j] = bigList[j],bigList[j-1]
+        if bigList[j - 1] > bigList[j]:
+            bigList[j - 1], bigList[j] = bigList[j], bigList[j - 1]
 print(bigList)
