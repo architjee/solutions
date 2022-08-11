1e7da04c1c1acb30f742f07ff1ecf9fa8e3b19c8
Archit Jain <architjee@gmail.com>
2022-08-11 19:57:34 +0530

    Successfully finished the complete A6 Assignment for Hashing.

---
+++ AlgoUniversity/Assignment 6/24 Consecutiveness.py
@@ -0,0 +1,22 @@
+n = int(input())
+bigList = list(map(int, input().split()))
+bigList.sort()
+prev = bigList[0]
+answer = 1
+# Now we will traverse in the array from 1st position.
+length = 1
+for i in range(1, n):
+
+    currentElement = bigList[i]
+    if currentElement==prev:
+        # Total Ignore.
+        pass
+    elif currentElement== prev+1:
+        length += 1
+        answer = max(answer, length)
+        prev = currentElement
+    else :
+        # Reset the length to 1.
+        length=1
+        prev =currentElement
+print(answer)
