14dcea762717d0276b468b27a5b98d28335a0fca
Archit Jain <architjee@gmail.com>
2022-08-19 03:39:37 +0530

    Solving problem Merge sort from SPOJ (128 Python)

---
+++ SPOJ/MERGSORT.py
@@ -0,0 +1,32 @@
+# The problem statement is  Simple.
+# Sort the numbers on the standard input using the merge sort algorithm.
+# Don't try to cheat by just calling your build in functions.
+def mergeTwoSmall(x, y):
+    z = []
+    i = 0
+    j = 0
+    while i < len(x) or j < len(y):
+        if j == len(y) or i < len(x) and x[i] <= y[j]:
+            z.append(x[i])
+            i += 1
+        else:
+            z.append(y[j])
+            j += 1
+    return z
+
+
+def mergeSort(array):
+    if(len(array)==1):
+        return array
+    if array:
+        # Then only we would like to perform some computation.
+        x = mergeSort(array[:len(array) // 2])
+        y = mergeSort(array[len(array) // 2:])
+        array = mergeTwoSmall(x, y)
+        return array
+    return array
+def main():
+    array= [100,16,15,124,36,93,456,15,34]
+    array =mergeSort(array)
+    print(array)
+main()
