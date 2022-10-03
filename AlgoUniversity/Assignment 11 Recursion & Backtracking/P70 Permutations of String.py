46a5e6aecb3c880678efaef7747a12a7cb2bef9b
Archit Jain <architjee@gmail.com>
2022-10-03 11:52:23 +0530

    Successfully solved Permutations of a String.py

--- AlgoUniversity/Assignment 11 Recursion & Backtracking/P70 Permutations of String.py
+++ AlgoUniversity/Assignment 11 Recursion & Backtracking/P70 Permutations of String.py
@@ -5,4 +5,29 @@ s = input()
 # s is the given string
 # also given in constraints that length of s <=8
 
-# Objective is to print all permutations of s in lexicographical order.
+listOfChars = list(s)
+result = []
+
+# Utility function to convert permutation to string.
+def convertPermToString(listOfChars):
+    return ''.join(listOfChars)
+
+def getAllPermutations(listOfChars, i):
+
+    # We will consider that we have already printed the s for i-1 characters,
+    # We are concerned with printing ith character and only the [i:] part of the list.
+    if i==len(listOfChars)-1:
+        result.append(convertPermToString(listOfChars))
+        return
+    for j in range(i, len(listOfChars)):
+        # Swapping i and j
+        listOfChars[i], listOfChars[j] = listOfChars[j], listOfChars[i]
+        getAllPermutations(listOfChars, i+1)
+
+        # Back Tracking
+        listOfChars[j], listOfChars[i] = listOfChars[i], listOfChars[j] 
+
+getAllPermutations(listOfChars, 0)
+result.sort()
+for combination in result:
+    print(combination)
