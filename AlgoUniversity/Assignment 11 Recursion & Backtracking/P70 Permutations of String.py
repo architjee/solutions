915a96106d757fd4d354c0a642a5742c96eec064
Archit Jain <architjee@gmail.com>
2022-10-03 07:40:32 +0530

    Attempting Permutations of String program

---
+++ AlgoUniversity/Assignment 11 Recursion & Backtracking/P70 Permutations of String.py
@@ -0,0 +1,8 @@
+# Program to print Permutations of a String. 
+
+s = input()
+
+# s is the given string
+# also given in constraints that length of s <=8
+
+# Objective is to print all permutations of s in lexicographical order.
