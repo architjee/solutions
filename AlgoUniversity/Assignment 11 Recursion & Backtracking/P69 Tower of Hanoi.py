d6c0f9de51ceb4646bd654330c992615ade7b447
Archit Jain <architjee@gmail.com>
2022-10-03 07:37:30 +0530

    Solved Tower of Hanoi Successfully, this solution can be used as a template

--- AlgoUniversity/Assignment 11 Recursion & Backtracking/P69 Tower of Hanoi.py
+++ AlgoUniversity/Assignment 11 Recursion & Backtracking/P69 Tower of Hanoi.py
@@ -1,2 +1,13 @@
 # Program to solve Graph Problem for solve program Tower of Hanoi. 
 
+n = int(input())
+
+def TowerOfHanoi(n, from_rod, to_rod, aux_rod):
+    if n==0:
+        return
+    TowerOfHanoi(n-1, from_rod, aux_rod, to_rod)
+    print(n,from_rod,to_rod)
+    TowerOfHanoi(n-1, aux_rod, to_rod, from_rod)
+
+print(pow(2, n)-1)
+TowerOfHanoi(n, 1, 2, 3)
