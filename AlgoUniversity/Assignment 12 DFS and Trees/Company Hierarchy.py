9b82e3f5b51542404624686231279c8d5e156a59
Archit Jain <architjee@gmail.com>
2022-10-19 15:12:50 +0530

    We have thought about the missing edge case, but unsuccessful till yet

--- AlgoUniversity/Assignment 12 DFS and Trees/Company Hierarchy.py
+++ AlgoUniversity/Assignment 12 DFS and Trees/Company Hierarchy.py
@@ -1,3 +1,38 @@
+#input vala section
 # We are given n
+from platform import node
+
+
 n = int(input())
+
 bossArray = list(map(int, input().split()))
+class Node:
+    def __init__(self, data = None):
+        self.key = data
+        self.child = []
+        self.underlings = 0
+ 
+i = 2
+nodeArray = [Node(x) for x in range(n+1)]
+for x in bossArray: 
+    nodeArray[x].child.append(nodeArray[i])
+    i+=1
+# We might implement some sort of DFS here:
+def dfsChildUnder(node, parent):
+    
+    if(node==None):
+        return 0
+    peopleUnderNode=0
+    
+    for x in node.child:
+        if x!=parent:
+            peopleUnderNode += dfsChildUnder(x, node)
+    node.underlings= peopleUnderNode
+    return peopleUnderNode+1
+dfsChildUnder(nodeArray[1], -1)
+for x in range(1, n+1):
+    print(nodeArray[x].underlings, end=" ")
+
+
+
+
