eaf80fdd50fb31bc107f60306c90c757aefa7701
Archit Jain <architjee@gmail.com>
2022-10-19 08:19:43 +0530

    Solved 137D Problem with Random Tests. The theory part is slightly still unclear, rest we should always rely on built-in functions to change strings to Integer and vice-versa

--- CodeForces/Education round 137/137D Problem with Random Tests.py
+++ CodeForces/Education round 137/137D Problem with Random Tests.py
@@ -1,55 +1,31 @@
-import enum
+def valOf(inp):
+    return int(inp, base=2)
 
-
-def convStringToVal(str):
-    resultant = 0
-    twoPow = 1
-    for j in range(-1, -1-len(str),-1):
-        if str[j]=='1':
-            resultant+=twoPow*1
-        twoPow*=2
-    return resultant
-
-def convValToBinary(val):
-    ans = ''
-    while val:
-        ans += str(val%2)
-        val //= 2
-    return ans[::-1]
-def indexOfFirstZero(s):
-    found = False
-    for index, x in enumerate(s):
-        if x=='0':
-            found = True
-            return found, index
-        
-    return found, -1
 def mainFunc():
     n = int(input())
     s = input()
-    value = convStringToVal(s)
-    if not value:
+    s = s.lstrip('0')
+    if not s:
+        # s is empty means we should throw zero at the system.
         print(0)
         return
-    s = convValToBinary(value);
+    zeroPos = -1
+    for index, value in enumerate(s):
+        if value=='0':
+            zeroPos=index
+            break
+    if zeroPos==-1:
+        
+        # That means there is no zero, which is a good thing because we can simply print s and be done with.
+        print(s)
+        return
+    expectedS2Size = len(s)-zeroPos
     
-    size = len(s)
-    # Now we have s with leading zeros dropped.
-    maxOR = value
-   
-    s1 = s
-    found, zeroPos =  indexOfFirstZero(s1)
-    if found:
-        expectedSizeS2 = size - zeroPos
-        # To check for every substring of length expectedSizeS2
-        for i in range(0, expectedSizeS2):
-            s2 = s[i:i+expectedSizeS2]
-            maxOR = max(maxOR, value|convStringToVal(s2))
-    else:
-        # Means zero is not found, which is good as well.
-        pass
-    print(convValToBinary(maxOR))
-
+    maxOrPossible = valOf(s)
+    for i in range(0, len(s)-expectedS2Size):
+        maxOrPossible = max(maxOrPossible, valOf(s[i:i+expectedS2Size])|valOf(s))   
+    print(bin(maxOrPossible).replace('0b', '')) 
+    return
 t = 1
 for testcase in range(t):
     mainFunc()
