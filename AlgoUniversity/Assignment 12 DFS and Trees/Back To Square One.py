b84e7dc5640a6739c8558fed67f31ea4c0bfd776
Archit Jain <architjee@gmail.com>
2022-10-21 05:00:16 +0530

    Back to Square One will be revisited later

--- AlgoUniversity/Assignment 12 DFS and Trees/Back To Square One.py
+++ AlgoUniversity/Assignment 12 DFS and Trees/Back To Square One.py
@@ -1,19 +1,13 @@
 
-global isCyclePresent
-isCyclePresent = False
 def mainFunc():
+    answer = False
     n, m = map(int, input().split())
-    adjList = [[] for i in range(n+1)]
+    adjList = [[] for i in range(n)]
     white, grey, black = range(3) 
     for edg in range(m):
-        if u==v:
-            global isCyclePresent
-            isCyclePresent = True
-            return
         u, v = map(int, input().split())
         adjList[u].append(v)
-    colors= [white for i in range(n+1)]
-    lastBlack=False
+    colors= [white for i in range(n)]
     def isCyclePresentViaDFS(adjList, root, colors):
         if colors[root]==grey:
             return True
@@ -23,30 +17,11 @@ def mainFunc():
             if isCyclePresentViaDFS(adjList, child, colors):
                 return True
         colors[root]=black
-        if root == n:
-            global lastBlack
-            lastBlack=True
         return False
-
-    def getFirstIndexOfWhite(colors):
-        for index, c in enumerate(colors):
-            if index==0:
-                continue
-            if c==white:
-                return True, index
-        return False, -1
-    isWhitePresent, indexOfWhite = getFirstIndexOfWhite(colors)
-    while not lastBlack and isWhitePresent:
-        if isCyclePresentViaDFS(adjList, indexOfWhite, colors): 
-            print(isCyclePresent)
-            global isCyclePresent
-            isCyclePresent= True
-            break
-        isWhitePresent, indexOfWhite = getFirstIndexOfWhite(colors)
+    if isCyclePresentViaDFS(adjList, 0, colors): 
+            answer=True
+    if answer:
+        print("NO")
     else:
-        return
+        print("YES")
 mainFunc()
-if isCyclePresent:
-    print("NO")
-else:
-    print("YES")
