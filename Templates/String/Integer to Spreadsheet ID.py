4fca96c3c41ffe9bbbc80fe60ac952d09866dac9
Archit Jain <architjee@gmail.com>
2022-11-07 18:42:46 +0530

    Trying to resolve the corner cases

--- Templates/String/Integer to Spreadsheet ID.py
+++ Templates/String/Integer to Spreadsheet ID.py
@@ -1,5 +1,5 @@
 import string
-upperCaseLetters = '0'+string.ascii_uppercase
+upperCaseLetters = string.ascii_uppercase
 # We are given the following test cases:
 # f(4)=>'D'
 # f(27)=>'AA'
@@ -8,10 +8,19 @@ upperCaseLetters = '0'+string.ascii_uppercase
 # f(1)=>'A'
 def f(num_as_int):
     def construct_from_base(num_as_int, base):
-        return '' if num_as_int==0 else construct_from_base(num_as_int//base, base)+upperCaseLetters[num_as_int%base]
+        if num_as_int==0:
+            return ''
+        appendage=''
+        if not num_as_int%base:
+            appendage='Z'
+        else:
+            appendage=upperCaseLetters[num_as_int%base-1  ]
+        return construct_from_base(num_as_int//base, base)+appendage
     return construct_from_base(num_as_int, 26)
 print(f(4))
+print(f(26))
 print(f(27))
+print(f(701))
 print(f(702))
 print(f(1))
 print(f(4))
