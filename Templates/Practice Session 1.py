f9124495ec7de41eae9e05a35540524ab8ab3034
Archit Jain <architjee@gmail.com>
2022-11-09 11:41:54 +0530

    Practiced ListNode and enumerate Flashcards

--- Templates/Practice Session 1.py
+++ Templates/Practice Session 1.py
@@ -3,3 +3,18 @@ int("1010101", base=2)
 
 # Converting int to binary string.
 bin(7).replace('0b','')
+
+iterable = [1,2,3,4,4]
+# Use of enumerate
+for index, value in enumerate(iterable):
+    print(index, value)
+
+d={1:"Alpha", 2:"Beta"}
+for key, value in d.items():
+    print(key , value)
+
+# General Linked List template
+class ListNode:
+    def __init__(self, data=0, next=None):
+        self.data= data
+        self.next = next
