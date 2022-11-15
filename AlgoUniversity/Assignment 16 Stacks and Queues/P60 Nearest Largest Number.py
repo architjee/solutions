f729c2532681065c03ea7259a73fcd0079c71c36
Archit Jain <32292295+architjee@users.noreply.github.com>
2022-11-15 09:24:33 +0530

    Successfully solved Nearest Largest Number using stacks

---
+++ AlgoUniversity/Assignment 16 Stacks and Queues/P60 Nearest Largest Number.py
@@ -0,0 +1,20 @@
+n = int(input())
+arr = list(map(int, input().split()))
+stk = []
+result = []
+# Consider an invariant
+# That elements in the dequeue will always be in the will always be of the form
+# 10 8 7 6 5
+for x in arr:
+    if not stk:
+        stk.append(x)
+        result.append(-1)
+    else:
+        while stk and stk[-1]<=x:
+            stk.pop()
+        if not stk:
+            result.append(-1)
+        else:
+            result.append(stk[-1])
+        stk.append(x)
+print(*result, sep=' ')
