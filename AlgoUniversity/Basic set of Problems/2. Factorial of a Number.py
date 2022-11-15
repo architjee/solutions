deb90311c5638d35b8cb75bc0c896ba49218a77d
Archit Jain <32292295+architjee@users.noreply.github.com>
2022-11-15 10:05:43 +0530

    Factorial of a Number

---
+++ AlgoUniversity/Basic set of Problems/2. Factorial of a Number.py
@@ -0,0 +1,10 @@
+def main():
+    n= int(input())
+    def fact(n):
+        if n==0 or n==1:
+            return 1
+        return n*fact(n-1)
+    print(fact(n))
+t = int(input())
+for _ in range(t):
+    main()
