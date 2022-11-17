3c3f39996c35d2c9f56bd46348a32fe4b2d9d863
Archit Jain <32292295+architjee@users.noreply.github.com>
2022-11-17 06:56:09 +0530

    Practicing Test if a tree is a height balanced.py

--- Templates/Binary Tree/Test if a tree is height balanced.py
+++ Templates/Binary Tree/Test if a tree is height balanced.py
@@ -1,4 +1,17 @@
 import collections
 def is_height_balanced(tree):
     Status= collections.namedtuple('Status',('is_balanced', 'height'))
-    
+    def is_height_balanced_helper(tree):
+        if not tree:
+            return Status(True, 0)
+        left_result = is_height_balanced_helper(tree.left)
+        if not left_result.is_balanced:
+            return left_result
+        right_result = is_height_balanced_helper(tree.right)
+        if not right_result.is_balanced:
+            return right_result
+        
+        is_balanced = abs(left_result.height-right_result.height) <= 1
+        height = max(left_result.height, right_result.height) + 1
+        return Status(is_balanced, height)
+    return is_height_balanced_helper(tree).is_balanced
