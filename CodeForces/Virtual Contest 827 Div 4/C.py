8bd81ce40024c437d7a14264da1f6669649d3065
Archit Jain <32292295+architjee@users.noreply.github.com>
2022-11-19 01:50:55 +0530

    Modified C of 827 according to the editorial

--- CodeForces/Virtual Contest 827 Div 4/C.py
+++ CodeForces/Virtual Contest 827 Div 4/C.py
@@ -10,28 +10,17 @@ def main():
     def is_pure_row(big_arr, i):
         row = big_arr[i]
         for element in row:
-            if element!=row[0]:
+            if element!='R':
                 return False
         return True
     
-    def is_pure_col(big_arr, j):
-        prevElement=big_arr[0][j]
-        for k in range(n):
-            element = big_arr[k][j]
-            if element!=prevElement:
-                return False
-            prevElement = element
-        return True
  
 
     # Iterate in all rows
     def get_first_pure_row_or_col_color(big_arr):
         for row_idx in range(n):
             if big_arr[row_idx][0]!='.' and is_pure_row(big_arr, row_idx):
-                return big_arr[row_idx][0]
-        for col_idx in range(n):
-            if big_arr[0][col_idx]!='.' and is_pure_col(big_arr, col_idx):
-                return big_arr[0][col_idx]
+                return 'R'
         return 'B'
     print(get_first_pure_row_or_col_color(big_arr))
 t = int(input())
