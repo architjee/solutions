72a38687c5e4c92977c9a95971213b8efa02f357
Archit Jain <32292295+architjee@users.noreply.github.com>
2022-12-07 21:56:21 +0530

    Solved 462 after seeing the solution

--- Leetcode/462. Minimum Moves to Equal Array Elements II.py
+++ Leetcode/462. Minimum Moves to Equal Array Elements II.py
@@ -1,19 +1,12 @@
 from typing import *
+
 class Solution:
     def minMoves2(self, nums: List[int]) -> int:
-        ## We just have to choose a number as target and find the difference between all elements to that target.
-        ## The number will always be between min and max that is certain.
-        ## The following is a brute force and unoptimized solution but just testing the waters
-        def getDifference(nums, target):
-            diff = 0
-            for ele in nums:
-                diff += abs(target-ele)
-            return diff
-        min_ele = min(nums)
-        max_ele = max(nums)
-        if min_ele == max_ele:
-            return 0
-        minPossible = 1000000007
-        for targetC in range(min_ele, max_ele):
-            minPossible = min(minPossible, getDifference(nums, targetC))
-        return minPossible
+        # The question is trying to mess with me,
+        # After looking at a solution, Now I know that I have to find a median,
+        nums.sort()
+        minMoves = 0
+        median = nums[len(nums)//2]
+        for num in nums:
+            minMoves += abs( num - median)
+        return minMoves
