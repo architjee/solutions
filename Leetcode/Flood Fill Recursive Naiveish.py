650a9d0315321566ef350888f83f2892316573cf
Archit Jain <32292295+architjee@users.noreply.github.com>
2022-12-11 11:07:47 +0530

    resolved pascal's triangle II in leetcode

--- Leetcode/Flood Fill Recursive Naiveish.py
+++ Leetcode/Flood Fill Recursive Naiveish.py
@@ -1,14 +1,18 @@
 from typing import *
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, incomingColor: int) -> List[List[int]]:
-        if image[sr][sc]==incomingColor:
-            return image;
-        def flood(image, sr, sc, testColor, replacementColor):
-            if sr>=0 and sr<len(image) and sc>=0 and sc<len(image[0]) and image[sr][sc]==testColor:
-                image[sr][sc]=replacementColor
-                flood(image, sr+1, sc, testColor, replacementColor)
-                flood(image, sr-1, sc, testColor, replacementColor)
-                flood(image, sr, sc+1, testColor, replacementColor)
-                flood(image, sr, sc-1, testColor, replacementColor)
-        flood(image, sr, sc, image[sr][sc], incomingColor)
-        return image
+    def generate(self, numRows: int) -> List[List[int]]:
+        result = []
+        result.append([1])
+        if(numRows==1):
+            return result
+        result.append([1,1]) 
+        if(numRows==2):
+            return result
+        for _ in range(2, numRows):
+            prev_row = result[-1]
+            new_row = [1]
+            for index in range(len(prev_row)-1):
+                new_row.append(prev_row[index]+prev_row[index+1])
+            new_row.append(1)
+            result.append(new_row)
+        return result
