90e47a899ddc415649e3a32b1f351d8ef1868393
Archit Jain <32292295+architjee@users.noreply.github.com>
2022-12-12 07:07:10 +0530

    Failed miserably

--- AlgoUniversity/DP/GIGAFACTORIES attempt 2.py
+++ AlgoUniversity/DP/GIGAFACTORIES attempt 2.py
@@ -9,12 +9,18 @@ profit_matrix = [[0]*(max_cost+1) for p in range(2)]
 if max_cost<=0:
     print(0)
 else:
-    def maximize_profit_under_max_cost(item_idx, max_cost):
-        for item_idx in range(1, n):
-            for allowed_cost in range(1,max_cost+1):
-                ## Becuase we have to consider == cost.                                         
-                profit_matrix[1][allowed_cost] = max(profit_matrix[1][allowed_cost-1]if cost_array[item_idx]>allowed_cost else profit_array[item_idx]+profit_matrix[1][allowed_cost-1], profit_matrix[0][allowed_cost])
-                                                                                                                                                                                        ## Don't consider that item and 
+    def maximize_profit_under_max_cost(n, max_cost):
+        for item_idx in range(0, n):
+            for allowed_cost in range(0,max_cost+1):
+                if item_idx==0 or allowed_cost==0:
+                    profit_matrix[1][allowed_cost] = 0
+                ## There can be only 2 cases :
+                c1=0
+                if cost_array[item_idx]<allowed_cost:
+                    c1 = profit_array[item_idx] + profit_matrix[0][allowed_cost]
+                c2 = profit_matrix[1][allowed_cost-1]
+                profit_matrix[1][allowed_cost] = max(c1,c2)
+                ## Becuase we have to consider == cost.                                                                                                                                                                                                             ## Don't consider that item and 
             profit_matrix[0]= profit_matrix[1].copy()
 
         return profit_matrix
