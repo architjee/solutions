c3fde5394f7115478b32a605f0f4361525a64ad0
Archit Jain <32292295+architjee@users.noreply.github.com>
2022-12-27 09:06:43 +0530

    comany hierarchy.py

--- AlgoUniversity/Assignment 12 DFS and Trees/Company Hierarchy.py
+++ AlgoUniversity/Assignment 12 DFS and Trees/Company Hierarchy.py
@@ -1,29 +1,48 @@
-n= int(input())
-boss_arr = list(map(int, input().split()))
-
-class Node:
-    def __init__(self, data = None):
-        self.employee_code = data
-        self.direct_underlings = []
-        self.team_size = 0
-
-nodeArray = [Node(x) for x in range(n+1)]
-visited = [False for x in range(n+1)]
-for index, boss in enumerate(boss_arr): 
-    if index>n:
-        break
-    nodeArray[boss].direct_underlings.append(index+2)
-def dfs_helper(node_idx):
-    node = nodeArray[node_idx]
-    if visited[node_idx]:
-        return node.team_size
-    team_size = 0
-    for x in node.direct_underlings:
-        team_size+= dfs_helper(x)
-    node.team_size = team_size
-    visited[node_idx] = True
-    return team_size+1
-dfs_helper(1)
-for i in range(1, n+1):
-    print(nodeArray[i].team_size ,end=' ')
-print()
+import sys
+ 
+from types import GeneratorType
+ 
+def bootstrap(f, stack=[]):
+    def wrappedfunc(*args, **kwargs):
+        if stack:
+            return f(*args, **kwargs)
+        else:
+            to = f(*args, **kwargs)
+            while True:
+                if type(to) is GeneratorType:
+                    stack.append(to)
+                    to = next(to)
+                else:
+                    stack.pop()
+                    if not stack:
+                        break
+                    to = stack[-1].send(to)
+            return to
+    return wrappedfunc
+ 
+ 
+ 
+n = int(sys.stdin.readline())
+adjacency_list_dir = [[] for i in range(n+1)]
+manages = [0 for i in range(n+1)]
+arr=[]
+if n!=1:
+    arr = [int(i) for i in sys.stdin.readline().split()]
+for i in range(len(arr)):
+    adjacency_list_dir[arr[i]].append(i+2)
+#print(adjacency_list_dir)
+ 
+@bootstrap
+def dfs(curr):
+    global adjacency_list_dir
+    global manages
+    sum = 0
+    for i in adjacency_list_dir[curr]:
+        sum+= yield dfs(i)
+    manages[curr]=sum
+    yield sum+1
+ 
+dfs(1)
+ 
+for i in range(1,n+1):
+    print(manages[i],end=' ')
