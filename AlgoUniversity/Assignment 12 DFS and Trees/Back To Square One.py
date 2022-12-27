00f68a2cc3df0f8b9230e60ac989b46d77c90da8
Archit Jain <32292295+architjee@users.noreply.github.com>
2022-12-27 09:25:40 +0530

    back to square one solved in python

--- AlgoUniversity/Assignment 12 DFS and Trees/Back To Square One.py
+++ AlgoUniversity/Assignment 12 DFS and Trees/Back To Square One.py
@@ -1,27 +1,31 @@
 
-def mainFunc():
-    answer = False
-    n, m = map(int, input().split())
-    adjList = [[] for i in range(n)]
-    white, grey, black = range(3) 
-    for edg in range(m):
-        u, v = map(int, input().split())
-        adjList[u].append(v)
-    colors= [white for i in range(n)]
-    def isCyclePresentViaDFS(adjList, root, colors):
-        if colors[root]==grey:
+WHITE, GRAY, BLACK = range(3)
+
+class GraphNode:
+    def __init__(self, val):
+        self.val = val
+        self.goto_edges = []
+        self.color = WHITE
+
+n, m = map(int, input().split())
+graph = [GraphNode(n) for x in range(n)]
+for edge_t in range(m):
+    u, v = map(int, input().split())
+    graph[u].goto_edges.append(graph[v])
+## We will solve this question using color theory
+
+def detect_cycle(G):
+    def has_cycle(cur):
+        if cur.color == GRAY:
             return True
-        colors[root] = grey
-        # We will start processing from 1 grey.
-        for child in adjList[root]:
-            if isCyclePresentViaDFS(adjList, child, colors):
-                return True
-        colors[root]=black
+        cur.color= GRAY
+        if any(next_node.color!=BLACK and has_cycle(next_node) for next_node in cur.goto_edges):
+            return True
+        cur.color = BLACK
         return False
-    if isCyclePresentViaDFS(adjList, 0, colors): 
-            answer=True
-    if answer:
-        print("NO")
-    else:
-        print("YES")
-mainFunc()
+    return any(node.color==WHITE and has_cycle(node) for node in G)
+
+if detect_cycle(graph):
+    print('NO')
+else:
+    print('YES')
