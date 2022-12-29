0fd539a58d74ef2cf9fce5ef9b3356b73eee2c65
Archit Jain <32292295+architjee@users.noreply.github.com>
2022-12-29 18:50:27 +0530

    attempt to make the solution dp but getting rte dont know why

--- GFG/is c interleaved by a and b.py
+++ GFG/is c interleaved by a and b.py
@@ -1,14 +1,22 @@
-
 class Solution:
     #function should return True/False
     def isInterleave(self, A, B, C):
+        dp = {}
         # Code here
         def aux_is_interleaved(a_idx, b_idx, c_idx):
+            key = a_idx, b_idx, c_idx
+            if key in dp:
+                return dp[key]
             if c_idx<=0 and (a_idx>0 or b_idx>0) :
+                dp[key] = False
                 return False
             if c_idx<=0:
+                dp[key] = True
                 return True
-            if (A[a_idx]==C[c_idx] and  aux_is_interleaved(a_idx-1, b_idx,c_idx-1)) or (B[b_idx]==C[c_idx] and aux_is_interleaved(a_idx, b_idx-1, c_idx-1)):
-                return True
-            return False
+            if key not in dp:
+                if (A[a_idx]==C[c_idx] and  aux_is_interleaved(a_idx-1, b_idx,c_idx-1)) or (B[b_idx]==C[c_idx] and aux_is_interleaved(a_idx, b_idx-1, c_idx-1)):
+                    dp[key] = True
+                else:
+                    dp[key]= False
+            return dp[key]
         return aux_is_interleaved(len(A)-1, len(B)-1, len(C)-1)
