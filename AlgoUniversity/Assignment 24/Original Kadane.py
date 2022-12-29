5d983cd5ccd2f7cecef5457f32afbdf3c0cc0ba2
Archit Jain <32292295+architjee@users.noreply.github.com>
2022-12-29 10:07:32 +0530

    solved original kadane problem algo u.py

--- AlgoUniversity/Assignment 24/Original Kadane.py
+++ AlgoUniversity/Assignment 24/Original Kadane.py
@@ -1,2 +1,11 @@
 n= int(input())
 arr = list(map(int, input().split()))
+min_sum , max_sum = 0, max(arr)
+import itertools
+for running_sum in itertools.accumulate(arr):
+    min_sum = min(min_sum, running_sum)
+    max_sum = max(max_sum, running_sum - min_sum)
+if not max_sum:
+    print(max(arr))
+else:
+    print(max_sum)
