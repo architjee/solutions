e7a769d5d10a97535c4167c0346a98540853d64b
Archit Jain <32292295+architjee@users.noreply.github.com>
2023-02-05 12:01:25 +0530

    quiz

---
+++ WorkAtTech/skillenza cp5.py
@@ -0,0 +1,9 @@
+size = int(input())
+nums = list(map(int,input().split()))
+nums = [-x for x in nums]
+import heapq
+heapq.heapify(nums)
+x = heapq.heappop(nums)
+y = heapq.heappop(nums)
+z = heapq.heappop(nums)
+print(-z, -y, -x)
